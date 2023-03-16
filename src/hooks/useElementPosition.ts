96dd0a5cf96755be510139669e06454652d4fbb8
KingDonggyu <dgkim69166916@gmail.com>
2023-03-16 19:38:49 +0900

    #2 feat(node): 컴포넌트 위치 반환 Hook 구현

---
+++ src/hooks/useElementPosition.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useElementPosistion = () => {
+  const ref = useRef<HTMLElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const rect = ref.current?.getBoundingClientRect();
+    if (rect) {
+      setPosition({
+        x: window.scrollX + rect.x + rect.width / 2,
+        y: window.scrollY + rect.y + rect.height / 2,
+      });
+    }
+  }, [ref]);
+
+  return { ref, position };
+};
+
+export default useElementPosistion;
