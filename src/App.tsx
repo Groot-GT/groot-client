645b5e3748d76efc382c9b4f76044da5f2cedde6
pinkishincoloragain <smb1103@gmail.com>
2023-03-23 17:41:48 +0900

    #4 feat: ThemeProvider  설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import Root from './components/organisms/Root';
 import { RootVariant } from './constants/node';
+import theme from './styles/theme';
 
 const App = () => (
   <RecoilRoot>
-    <Root rootVariant={RootVariant.BOTH_SIDE} />
+    <ThemeProvider theme={{ theme }}>
+      <Root rootVariant={RootVariant.BOTH_SIDE} />
+    </ThemeProvider>
   </RecoilRoot>
 );
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+const fontSizes = {};
+
+const colors = {};
+
+const common = {
+  flexCenter: `
+    display: flex;
+    justify-contents: center;
+    align-items: center;
+  `,
+  flexCenterColumn: `
+    display: flex;
+    flex-direction: column;
+    justify-contents: center;
+    align-items: center;
+  `,
+};
+
+const theme = {
+  fontSizes,
+  colors,
+  common,
+};
+
+export default theme;
