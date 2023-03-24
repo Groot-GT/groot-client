ba8a77891f16c34a7dcd1ae65740ba4118f8718a
pinkishincoloragain <smb1103@gmail.com>
2023-03-25 06:34:07 +0900

    #4 feat: asset export를 위한 asset.ts 추가

---
+++ src/components/atoms/ToggleButton/asset.ts
@@ -0,0 +1,7 @@
+import chevronDown from './ToggleButtonAssets/chevron-down.svg';
+import chevronTop from './ToggleButtonAssets/chevron-top.svg';
+
+export const ToggleButtonAssets = {
+  chevronDown,
+  chevronTop,
+};
