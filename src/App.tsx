0700cd3a7590979004a0e32081bc3fc161b6acdd
pinkishincoloragain <smb1103@gmail.com>
2023-03-26 00:43:29 +0900

    #6 feat: Drawing Page의 routing을 App.tsx에서 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Draw from './components/pages/Draw';
+
 import GlobalStyle from './styles/global';
-// import Root from './components/organisms/Root';
-// import { RootVariant } from './constants/node';
 import theme from './styles/theme';
-import Button from './components/atoms/Button';
-import ToggleButton from './components/atoms/ToggleButton';
+// import Button from './components/atoms/Button';
+// import ToggleButton from './components/atoms/ToggleButton';
 
 const App = () => (
   <RecoilRoot>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      {/* <Root rootVariant={RootVariant.BOTH_SIDE} /> */}
-      <Button>Example Button</Button>
-      <ToggleButton onClick={(e) => console.log(e)} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Draw />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </RecoilRoot>
 );
---
+++ src/components/pages/Draw.tsx
@@ -0,0 +1,6 @@
+import { RootVariant } from 'src/constants/node';
+import Root from '../organisms/Root';
+
+const Draw = () => <Root rootVariant={RootVariant.BOTH_SIDE} />;
+
+export default Draw;
