b7f3fe54917fea76a1927c3383dbe788df91d1c6
pinkishincoloragain <smb1103@gmail.com>
2023-03-28 05:33:45 +0900

    #6 refactor: atom/ToggleButton의 icon assets를 src/assets로 이동

--- src/components/atoms/ToggleButton/asset.ts
+++
@@ -1,7 +0,0 @@
-import chevronDown from './ToggleButtonAssets/chevron-down.svg';
-import chevronTop from './ToggleButtonAssets/chevron-top.svg';
-
-export const ToggleButtonAssets = {
-  chevronDown,
-  chevronTop,
-};
