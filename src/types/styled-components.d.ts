ac3027eb4855da37c85ba2d1dfaf5730cb8aece9
KingDonggyu <dgkim69166916@gmail.com>
2023-04-02 14:34:28 +0900

    #8 refactor: styled-components DefaultTheme 타입 정의 자동화

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,66 +1,9 @@
 import 'styled-components';
+import theme from 'src/styles/theme';
+
+type Theme = typeof theme;
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    fontSizes: {
-      subtitle1: string;
-      subtitle2: string;
-      body1: string;
-      body2: string;
-      overline: string;
-    };
-    fontWeights: {
-      light: number;
-      regular: number;
-      medium: number;
-    };
-    colors: {
-      main: {
-        0: string;
-        1: string;
-        2: string;
-      };
-      white: {
-        0: string;
-      };
-      black: {
-        0: string;
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-      };
-      red: {
-        1: string;
-        2: string;
-      };
-      blue: {
-        1: string;
-        2: string;
-      };
-      yellow: {
-        1: string;
-        2: string;
-      };
-      green: {
-        1: string;
-        2: string;
-      };
-    };
-    mixins: {
-      shadow: string;
-      flexCenter: string;
-      flexCenterColumn: string;
-      flexCenterRow: string;
-      subtitle1: string;
-      subtitle2: string;
-      body1: string;
-      body2: string;
-      overline: string;
-    };
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
 }
