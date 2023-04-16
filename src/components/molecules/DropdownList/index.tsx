671eda328d72c69d33fc5006ff85184f6e5ad50d
pinkishincoloragain <smb1103@gmail.com>
2023-04-16 17:54:35 +0900

    #11 feat: Dropdown에 DropdownList 구현

---
+++ src/components/molecules/DropdownList/index.tsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import DropdownOption from 'src/components/atoms/DropdownOption';
+import Divider from 'src/components/atoms/Divider';
+import * as s from './style';
+
+type DropdownListProps = {
+  items: string[];
+  dropdownWidth: number | undefined;
+  handleOptionClick: (idx: string) => void;
+  icons?: string[] | undefined;
+}
+
+const defaultProps = {
+  icons: undefined,
+};
+
+const DropdownList = ({ items, dropdownWidth, icons, handleOptionClick }: DropdownListProps) => (
+  <s.DropdownListWrapper width={dropdownWidth}>
+    {items.map((item, idx) => (
+      <Fragment key={item}>
+        <DropdownOption value={item} icon={icons ? icons[idx] : null}
+                        onClick={() => handleOptionClick(item)} />
+        <Divider vertical={false} length={100} />
+      </Fragment>
+    ))}
+  </s.DropdownListWrapper>
+);
+
+DropdownList.defaultProps = defaultProps;
+
+export default DropdownList;
