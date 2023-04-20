42a9161cf9861912951e48234e52cd7a1449d084
pinkishincoloragain <smb1103@gmail.com>
2023-04-20 14:32:26 +0900

    #11 feat: Black dot icon 추가, Icon component에 변경 사항 추가

--- src/assets/asset.ts
+++ src/assets/asset.ts
@@ -2,10 +2,16 @@ import chevronDown from './icons/chevron-down.svg';
 import chevronTop from './icons/chevron-top.svg';
 import tree from './icons/tree.svg';
 import search from './icons/search.svg';
+import close from './icons/close.svg';
+import formatLeft from './icons/format-align-left.svg';
+import blackDot from './icons/black-dot.svg';
 
 export const ToggleButtonAssets = {
   chevronDown,
   chevronTop,
   tree,
   search,
+  close,
+  formatLeft,
+  blackDot,
 };
--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -1,9 +1,6 @@
 import { ToggleButtonAssets } from 'src/assets/asset';
+import { IconType } from 'src/types/icon';
 
-
-// type IconProps = typeof ToggleButtonAssets[keyof typeof ToggleButtonAssets];
-type IconProps = keyof typeof ToggleButtonAssets;
-
-const Icon = ({ iconImg }: { iconImg: IconProps }) =>
+const Icon = ({ iconImg }: { iconImg: IconType }) =>
   <img src={ToggleButtonAssets[iconImg]} alt={iconImg} />;
 export default Icon;
