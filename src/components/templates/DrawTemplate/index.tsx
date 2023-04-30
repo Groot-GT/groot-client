52c9ffb45db4ddc27eae159e09a0087467f36b21
pinkishincoloragain <smb1103@gmail.com>
2023-05-01 03:10:28 +0900

    Revert "#16 style: DrawTemplate 상단과 하단을 나누고, 그 다음 좌우를 나누는 것으로 스타일 변경"
    This reverts commit 4bf69f7f462a138181b8459d0722d72d31d4291e.

--- src/components/templates/DrawTemplate/index.tsx
+++ src/components/templates/DrawTemplate/index.tsx
@@ -1,6 +1,5 @@
 import ProjectPanel from 'src/components/organisms/ProjectPanel';
 import StatusPanel from 'src/components/organisms/StatusPanel';
-import CurrentUsersPanel from 'src/components/organisms/CurrentUsersPanel';
 import { LeftBar, RightBar } from 'src/components/organisms/SideBar';
 import Root from 'src/components/organisms/Root';
 import { RootVariant } from 'src/constants/node';
@@ -8,27 +7,15 @@ import * as s from './style';
 
 const DrawTemplate = () => (
   <s.DrawTemplateWrapper>
-    {/* Top side */}
-    <s.TopWrapper>
+    <s.SideWrapper>
       <ProjectPanel projectName='Untitled_1' />
-      <s.TopRightWrapper>
-        <CurrentUsersPanel />
-        <StatusPanel />
-      </s.TopRightWrapper>
-    </s.TopWrapper>
-
-    {/* Middle side */}
-    <s.MiddleWrapper>
-      {/* Left sidebar */}
-      <s.SideWrapper>
-        <LeftBar />
-      </s.SideWrapper>
-      <Root rootVariant={RootVariant.BOTH_SIDE} />
-      {/* Right sidebar */}
-      <s.SideWrapper>
-        <RightBar />
-      </s.SideWrapper>
-    </s.MiddleWrapper>
+      <LeftBar />
+    </s.SideWrapper>
+    <Root rootVariant={RootVariant.BOTH_SIDE} />
+    <s.SideWrapper>
+      <StatusPanel />
+      <RightBar />
+    </s.SideWrapper>
   </s.DrawTemplateWrapper>
 );
 
--- src/components/templates/DrawTemplate/style.ts
+++ src/components/templates/DrawTemplate/style.ts
@@ -2,33 +2,17 @@ import styled from 'styled-components';
 
 export const DrawTemplateWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
   width: 100vw;
   height: 100vh;
   padding: 19px 31px;
   background-color: ${({ theme }) => theme.colors.black[2]};
 `;
 
-export const TopWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 16px;
-`;
-
-export const TopRightWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const MiddleWrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
 export const SideWrapper = styled.div`
   z-index: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 `;
