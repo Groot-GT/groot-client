368f1886a009271df2f11ebd46b981d1f24281aa
pinkishincoloragain <smb1103@gmail.com>
2023-05-01 03:12:58 +0900

    #16 refactor: ColorComponent 내에 child 추가 가능, hover 시 border 생성 가능하게 변경

--- src/components/atoms/ColorPanel/index.tsx
+++ src/components/atoms/ColorPanel/index.tsx
@@ -1,18 +1,23 @@
+import { ReactNode } from 'react';
 import theme from 'src/styles/theme';
 import * as s from './style';
 
 type ColorPanelProps = {
-  color: keyof typeof theme.nodeColors;
+  color: keyof typeof theme.titleColors;
   onClick?: () => void;
+  children?: ReactNode;
 }
 
 const defaultProps = {
   onClick: () => {
   },
+  children: null,
 };
 
-const ColorPanel = ({ color, onClick }: ColorPanelProps) =>
-  <s.ColorPanel onClick={onClick} color={color} />;
+const ColorPanel = ({ color, onClick, children }: ColorPanelProps) =>
+  <s.ColorPanel onClick={onClick} color={color}>
+    {children}
+  </s.ColorPanel>;
 
 ColorPanel.defaultProps = defaultProps;
 
