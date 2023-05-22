45a2bcf94065ca6eb631680382f4ade07690d149
pinkishincoloragain <smb1103@gmail.com>
2023-05-22 21:25:58 +0900

    #22 style: ProjectSortOptionPanel Workspace에 추가

--- src/components/organisms/WorkspaceProjectBorads/index.tsx
+++ src/components/organisms/WorkspaceProjectBorads/index.tsx
@@ -1,32 +1,18 @@
 import { useRecoilValue } from 'recoil';
-import { projectCreatedAtDatesSelector, projectOwnersSelector } from 'src/recoil/projectState';
-import Dropdown from 'src/components/organisms/Dropdown';
-import IconButton from 'src/components/molecules/IconButton';
+import { sortedProjectsSelector } from 'src/recoil/projectsState';
 import Item from 'src/components/molecules/Item';
-import useSortedProjectsByOption from 'src/hooks/useSortedProjectsByOption';
+import ProjectSortOptionPanel from 'src/components/organisms/ProjectSortOptionPanel';
 import * as s from './style';
 
 const WorkspaceProjectBorads = () => {
-  const projectOwners = useRecoilValue(projectOwnersSelector);
-  const projectCreatedAtDates = useRecoilValue(projectCreatedAtDatesSelector);
-  const sortedProjects = useSortedProjectsByOption('createdAt');
-
+  const sortedProjects = useRecoilValue(sortedProjectsSelector);
+  
   return (
     <s.BoardsWrapper>
-      <s.OptionsWrapper>
-        <s.TitleWrapper>
-          Project Boards
-        </s.TitleWrapper>
-        <Dropdown items={projectOwners} />
-        <Dropdown items={projectCreatedAtDates} />
-        <IconButton icon='menu' onClick={() => {
-        }} />
-        <IconButton icon='viewBoxes' onClick={() => {
-        }} />
-      </s.OptionsWrapper>
+      <ProjectSortOptionPanel />
       {Object.entries(sortedProjects).map(([projectId, project]) => (
-        <Item key={projectId} itemTitle={project.name} deleteItem={undefined} />))
-      }
+        <Item key={projectId} itemTitle={project.name} deleteItem={undefined} />
+      ))}
     </s.BoardsWrapper>
   );
 };
--- src/components/organisms/WorkspaceProjectBorads/style.ts
+++ src/components/organisms/WorkspaceProjectBorads/style.ts
@@ -8,16 +8,3 @@ export const BoardsWrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `;
-
-export const TitleWrapper = styled.div`
-  ${({ theme }) => theme.mixins.subtitle1}
-`;
-
-export const OptionsWrapper = styled.div`
-  max-width: 80%;
-  height: fit-content;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
