f4f78f86234613337793e922d8387bc33fa2ad1f
pinkishincoloragain <smb1103@gmail.com>
2023-05-23 02:42:42 +0900

    #23 bug: RootVariant enum에 vertical, horizontal 추가, Sidebar 연동

--- src/recoil/nodeThemeState.ts
+++ src/recoil/nodeThemeState.ts
@@ -1,8 +1,9 @@
 import { atom, selector } from 'recoil';
 import theme from 'src/styles/theme';
+import { RootVariant } from '../constants/node';
 
 type NodeThemeType = keyof typeof theme.nodeColors;
-type NodeThemeValueType = typeof theme.nodeColors[NodeThemeType];
+type NodeThemeValueType = (typeof theme.nodeColors)[NodeThemeType];
 
 const defaultTheme = 'yellow';
 
@@ -11,9 +12,14 @@ const nodeThemeState = atom<NodeThemeType>({
   default: defaultTheme,
 });
 
+export const nodeRootVariantState = atom<RootVariant>({
+  key: 'nodeRootVariantState',
+  default: RootVariant.BOTH_SIDE,
+});
+
 export const nodeThemeSelector = selector<NodeThemeValueType>({
   key: 'nodeThemeSelector',
-  get: ({ get }) => (theme.nodeColors[get(nodeThemeState)]),
+  get: ({ get }) => theme.nodeColors[get(nodeThemeState)],
 });
 
 export default nodeThemeState;
