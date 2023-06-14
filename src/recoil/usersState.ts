f8d4cc77160a1cd295aecb9e7f5fe06b2a091e0b
pinkishincoloragain <smb1103@gmail.com>
2023-06-14 21:10:46 +0900

    #27 chore: TODO 삭제 - 추후 서버 동기화 시 적용 예정 반영

--- src/recoil/usersState.ts
+++ src/recoil/usersState.ts
@@ -2,8 +2,6 @@ import { atom } from 'recoil';
 import { currentUser, users } from 'src/mock';
 import { User, Users } from 'src/types/user';
 
-// #TODO: Project별로 Online User를 관리해야 하는지 백엔드와 논의
-
 const onlineUsersState = atom<Users>({
   key: 'usersState',
   default: users,
